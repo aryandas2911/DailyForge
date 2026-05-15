629c61b138723071dc445704ce5f380c0be05007
Pratik346 <pratikkumarmohanty9@gmail.com>
2026-05-15 18:04:36 +0530

    fix: resolve merge conflict

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 
 // create axios instance
-/*const api = axios.create({
+const api = axios.create({
   baseURL: "https://dailyforge-backend.onrender.com/api/",
   timeout: 2000,
-});*/
-const api = axios.create({
-  baseURL: "http://localhost:5000/api/",
 });
-
 // attach jwt automatically with each request
 api.interceptors.request.use((config) => {
   try {
