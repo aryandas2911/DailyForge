cbf5d5d583981da427688fb9faa37e55b4ff9a80
Hamssini <143865437+n1o5@users.noreply.github.com>
2026-05-30 11:36:05 +0530

    Removed duplicate email and password from user schema
    Removed email and password fields from user schema.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,9 +2,6 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, trim: true},
-    email: { type: String, required: true, unique: true , trim: true},
-    password: { type: String, required: true },
     name: {
       type: String,
       required: true,
